5db5e5723bc48d88fea8f7bc748df947366b3447
sjbalint <anonkafka@protonmail.com>
2021-09-23 17:00:32 -0400

    small comments

--- 09_23_2021.R
+++ 09_23_2021.R
@@ -10,3 +10,18 @@ tm_shape(counties) +
   tm_polygons("area", 
               style="quantile", 
               title="Area of Rhode Island Counties")
+
+
+library(ggplot2)
+# Check our coordinate reference systems
+st_crs(gages_sf)
+st_crs(pnw)
+# Are they equal?
+st_crs(gages_sf)==st_crs(pnw)
+# transform one to the other
+gages_sf <- st_transform(gages_sf, st_crs(pnw)) #instead of passing ECSG code, transform to coordinate system of states data
+ggplot() + 
+  geom_sf(data=gages_sf,  color="blue") +
+  geom_sf(data=pnw,  color="black", fill=NA) +
+  labs(title="USGS Stream Gages in the Pacific Northwest") +
+  theme_bw() 
